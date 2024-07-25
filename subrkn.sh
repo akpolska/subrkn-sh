bba70d64f050088c171d92cebcc453ed06ab9b12
akpolska <68820375+akpolska@users.noreply.github.com>
2024-07-25 19:03:00 +0530

    Add files via upload

---
+++ subrkn.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then 
+	echo './subrkn.sh <list of domains>'
+	exit 1
+fi
+
+echo '---Finding subdoamin---'
+while read line 
+do 
+	for var in $line
+	do
+		echo '---SCANNING---' $var
+
+		subfinder -silent -d $var > out1
+		cat out1 >> subs1
+
+		assetfinder -subs-only $var > out2
+		cat out2 >> subs2
+
+		rm out1 out2 
+
+	done
+done < $1
+
+#combine the results 
+
+sort -u subs1 subs2 > allsubs
+rm subs1 subs2 
+echo 'SAVED SUBDOMAINS TO FILES "allsubs"'
+echo 'FINDING LIVE HOSTS'
+cat allsubs |httprobe > live-subs
+echo 'Live hosts downloaded'
+echo 'COMPLETED! CHECK THE FILES named as "live-subs"'
