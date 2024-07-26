58029cf2e68b681f93cbbf25e46b3d9a5626606a
SKAV <68820375+akpolska@users.noreply.github.com>
2024-07-26 15:05:25 +0530

    Update subrkn.sh
    small changes

--- subrkn.sh
+++ subrkn.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-
+#basic script to recon and check host are live or not
+#checking
 if [ -z $1 ]
 then 
-	echo './subrkn.sh <list of domains>'
+	echo './subrkn.sh ||list of Domains||'
 	exit 1
 fi
 
-echo '---Finding subdoamin---'
+echo '||---Finding Subdomains---||'
 while read line 
 do 
 	for var in $line
 	do
-		echo '---SCANNING---' $var
+		echo '||---SCANNING---||' $var
 
 		subfinder -silent -d $var > out1
 		cat out1 >> subs1
@@ -24,12 +25,12 @@ do
 	done
 done < $1
 
-#combine the results 
+#combining the results 
 
 sort -u subs1 subs2 > allsubs
 rm subs1 subs2 
-echo 'SAVED SUBDOMAINS TO FILES "allsubs"'
+echo '||SAVED SUBDOMAINS TO FILES "allsubs"||'
 echo 'FINDING LIVE HOSTS'
 cat allsubs |httprobe > live-subs
-echo 'Live hosts downloaded'
-echo 'COMPLETED! CHECK THE FILES named as "live-subs"'
+echo '||Live hosts downloaded||'
+echo '||COMPLETED! CHECK THE FILES named as "live-subs"||'
